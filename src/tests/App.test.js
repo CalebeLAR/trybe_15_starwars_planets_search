49ae4d91b3246fbd95417ac54c50793eae952682
CalebeLAR <ca6ele@gmail.com>
2023-01-07 17:24:37 -0400

    refat

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,42 +1,23 @@
 import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
 import App from '../routes/App';
+import userEvent from '@testing-library/user-event';
 
-describe('App.js testes para ver se os elementos estão aparecendo na tela.\n',() => {
-  beforeEach(() => {
-    render(<App/>);
-    waitForElementToBeRemoved(()=> screen.getByText('Loading...'))
-  });
-  // test.only('only', ()=> {});
-  test.todo('01) verifica se foi feita uma requisição para a API starWarsPlanets')
-  test('02) deve haver um titulo com o texto "SEARCH PLANET"', () => {
-    expect(screen.getByText("SEARCH PLANET")).toBeInTheDocument();
-  });
-  test.todo('verifica se ao digitar na barra de pesquisa os planetas são filtrados pelo nome');
-  test('03) deve haver um input para pesquisar o nome dos planetas', () => {
-    expect(screen.getByTestId('name-filter')).toBeInTheDocument();
-  });
-  test('1) deve haver um input com o texto "Coluna"', () => {
-    expect(screen.getByTestId('column-filter')).toBeInTheDocument();
-  });
-  test('1) deve haver um input com o texto "Operador"', () => {
-    expect(screen.getByTestId('comparison-filter')).toBeInTheDocument();
-  });
-  test('1) deve haver um input com o texto "Valor"', () => {
-    expect(screen.getByTestId('value-filter')).toBeInTheDocument();
-  });
-  test('1) deve haver um botão com o texto "Filtrar"', () => {
-    expect(screen.getByTestId('button-filter')).toBeInTheDocument();
-  });
-  test('1) tabela deve possuir 13 colunas com as informações de cada planeta\n', () => {
-    const table = screen.getByRole('table');
-    const columns = table.getElementsByTagName('th');
-    const arr = [
-      'Name','Rotation','Orbital_Period', 'Diameter','Climate','Gravity','Terrain','Surface_Water','Population','Films','Created','Edited','url'
-    ];
-    [...columns].forEach((element)=>{
-      const isIncludes = arr.includes(element.textContent);
-      expect(isIncludes).toBeTruthy();
-    })
-    expect(columns).toHaveLength(13);
-  });
+describe('ao iniciar a plicação.', ()=> {
+  test.todo('deve ser renderizada uma tabela só com uma linha contendo 13 colunas')
+  test.todo('cada linha da tabela deve ter uma caracteristica de um planeta')
+  test.todo('deve aparecer uma mensagen de loading antes de carregar os planetas da tabela')
+  test.todo('deve ser feita uma requisição para a API de planetas do starWars')
+  test.todo('apos a requisição a tabela tem que ser abastecida com os planetas')
 })
+
+describe('search barr', ()=> {
+  test.todo('deve ser rederizada uma search barr para pesquisar os planetas pelo nome');
+  test.todo('ao digitar na search barr, os planetas precisam ser filtrados instantaneamente na tabela');
+  test.todo('caso não seja encontrado nem um planeta, a tabela tem que ficar vazia');
+})
+
+describe('filtros numéricos', ()=> {
+  test.todo('deve haver tres inputs para filtrar os planetas. cada um com o texto: coluna, comparação e valor.');
+  test.todo('ao clicar no botão de filtro, a tabela deve filtrar pelos inputs de filtro');
+  test.todo('o filtro por nome deve continuar nos planetas filtrados');
+});
